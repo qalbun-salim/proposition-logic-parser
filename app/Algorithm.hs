65539acac6461bde64c48df24978115ae0aa7178
Qalbun Salim Buanaputra <qalbunsb@gmail.com>
2023-12-17 20:22:38 +0700

    feat:update solver algorithm to support more rules

--- app/Algorithm.hs
+++ app/Algorithm.hs
@@ -7,16 +7,34 @@ import qualified Data.Set as Set
 
 pairwise :: [a] -> [(a, a)]
 pairwise [] = []
-pairwise (x:xs) = map (\y -> (x, y)) xs ++ pairwise xs
+pairwise (x:xs) = [(x, y) | y <- xs] ++ pairwise xs
 
-computeSolution :: (Prop,Prop) -> [Prop]
-computeSolution x = filter (/= F) lst where
-    lst = map ($ x) (fmap uncurry [modusTollens,modusPonens,silogismeHipotetik,silogismeDisjungtif,resolusi])
+-- computeSolution :: (Prop,Prop) -> [Prop]
+-- computeSolution x = filter (/= F) lst where
+--     lst = map ($ x) (fmap uncurry [modusTollens,modusPonens,silogismeHipotetik,silogismeDisjungtif,resolusi])
 
-flattenSolution :: [Prop] -> [Prop]
-flattenSolution x = concatMap computeSolution (pairwise x)
+computeSolution :: [Prop] -> [Prop]
+computeSolution x = filter (/= F) $ solverThree x ++ solverTwo x ++ solverOne x 
+
+solveOneProp :: [Prop->Prop] -> [Prop] -> [Prop]
+solveOneProp props prems = props <*> prems
+
+solverOne =  solveOneProp [simplifikasi]
+
+solveTwoProps :: [Prop->Prop->Prop] -> [Prop] -> [Prop]
+solveTwoProps props prems = props <*> prems <*> prems
+
+solverTwo = solveTwoProps [modusTollens,modusPonens,silogismeHipotetik,silogismeDisjungtif,resolusi,instansiasiUniversal,generalisasiEksistensial]
+
+solveThreeProps :: [Prop->Prop->Prop->Prop] -> [Prop] -> [Prop]
+solveThreeProps props prems = props <*> prems <*> prems <*> prems
+
+solverThree = solveThreeProps [modusPonensUniversal]
+
+-- flattenSolution :: [Prop] -> [Prop]
+-- flattenSolution x = concatMap computeSolution (pairwise x)
 
 calculateSolution :: [Prop] -> Prop -> Bool
-calculateSolution premises hyp | hyp `elem` (premises ++ flattenSolution premises) = True
-                       | Set.fromList premises == Set.fromList (premises ++ flattenSolution premises)  = False
-                       | otherwise = calculateSolution (premises ++ flattenSolution premises) hyp
+calculateSolution premises hyp | hyp `elem` (premises ++ computeSolution premises) = True
+                       | Set.fromList premises == Set.fromList (premises ++ computeSolution premises)  = False
+                       | otherwise = calculateSolution (premises ++ computeSolution premises) hyp
